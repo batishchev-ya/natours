6b409fe11c2e9f06abe19ac238b47a9fd02fbc7f
Yaroslav Batishchev <yaroslav.batishchev1@gmail.com>
2022-07-13 22:49:51 +0200

    Implemented stripe messages 3

--- controlers/viewsController.js
+++ controlers/viewsController.js
@@ -4,7 +4,7 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-exports.alerts = (res, req, next) => {
+exports.alerts = (req, res, next) => {
   const { alert } = req.query;
   if (alert === 'booking') {
     res.locals.alert =
